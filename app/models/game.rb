5d3ac0b963adbb02f0de1678f2edd4b6aa7acb2f
Brandon Castro <castroyou999@gmail.com>
2023-01-25 12:51:32 -0500

    commit tree f90c25e778a8d2fd40906f49ff8551b9bc0c33d2

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ActiveRecord::Base
-  
+    # def reviews
+    #     Review.where(game_id: self.id)
+    # end
+    has_many :reviews
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ActiveRecord::Base
-  
+    # def game
+    #      Game.find(self.game_id)
+    # end
+    belongs_to :game
 end
